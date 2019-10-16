c4a7aab38dd522ea528294b2ea8e5642282774ef
Michael Schaefer <schaefer.michael@de.ibm.com>
2019-10-16 10:31:04 +0300

    WIP: make errand deployable with cf-deployment
    [#168979143]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,19 +5,12 @@ set -e
 bosh -e vbox deploy -d eirini-installer manifest.yaml \
   -o operations/eirini.yaml \
   -o operations/bpm.yml \
-  -v ingress_endpoint="not_set" \
   -v use_registry_ingress=false \
-  -v registry_address="not_set" \
-  -v cc_internal_api="not_set" \
-  -v cc_certs_secret_name="not_set" \
-  -v nats_address="not_set" \
-  -v doppler_address="not_set" \
-  -v doppler-cert="not_set" \
-  -v doppler-cert-key="not_set" \
-  -v cc-server-crt="not_set" \
-  -v cc-server-crt-key="not_set" \
-  -v eirini-client-crt="not_set" \
-  -v eirini-client-crt-key="not_set" \
-  -v internal-ca-cert="not_set" \
+  -v registry_address="http://some-registry.somewhere.io" \
+  -v ingress_endpoint="whatever" \
+  -v cc_internal_api="https://cloud-controller-ng.service.cf.internal:9023" \
+  -v cc_certs_secret_name="cc-certs" \
+  -v nats_address="q-s0.nats.default.cf.bosh" \
+  -v doppler_address="doppler.service.cf.internal:8082" \
   -v kube_config_flatten="$(kubectl config view --flatten=true)" \
   --vars-store deployment-vars.yml
